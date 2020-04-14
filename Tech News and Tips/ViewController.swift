cf20be34d5d5ee8746f5a4dcd010af156525fb7e
Kiran <kirank9695@yahoo.com>
2020-04-14 22:49:43 +0530

    Initial Commit

---
+++ Tech News and Tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tech News and Tips
+//
+//  Created by kiran on 14/04/20.
+//  Copyright © 2020 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
